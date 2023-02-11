6b139d132b1322d062b0984b1e8058fbfab6a51f
Peleg Levy <89268094+Peleg07@users.noreply.github.com>
2023-02-11 21:16:40 +0200

    Delete app directory

--- app/backend/database/db_mongo.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-
-#Connect to Database
-client = pymongo.MongoClient("mongodb://database:27017")
-database = client["todo_app"]
-
-#Database Collections
-tasks_db = database["tasks"]
